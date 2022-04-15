1f851fa4fce9056f22f11604847febcd915bf2f2
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-04-15 14:52:14 -0300

    feat: prepare server with express

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express'
+
+const app = express();
+const port = 3000
+
+app.get('/', (request: Request, response: Response) => {
+  response.status(200).json({
+    body: {
+      message: 'Server running'
+    }
+  })
+})
+
+app.listen(port, () => console.log(`server running in http://localhost:${port}`))
+
+
